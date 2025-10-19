bc0fa29d9949ea9383946b218750d0c5c39dd635
aslanchan-sdsc <aslanchan7@gmail.com>
2025-10-19 11:31:11 -0700

    added temporary art

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -1,12 +1,19 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class ScoreUI : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI comboText;
     [SerializeField] TextMeshProUGUI multText;
 
+    [Header("Tweening")]
+    [SerializeField] float scaleTime;
+    [SerializeField] float countTime;
+    private int currScore;
+    
     void Start()
     {
         if (scoreText == null)
@@ -27,17 +34,69 @@ public class ScoreUI : MonoBehaviour
 
     void Update()
     {
-        scoreText.text = "Score: " + ScoreManager.Instance.Score;
+        UpdateScoreText();
 
+        UpdateComboText();
+
+        multText.text = $"Multiplier: x{ScoreManager.Instance.ScoreMult:F1}";
+    }
+
+    void UpdateScoreText()
+    {
+        string newText = $"Score: {ScoreManager.Instance.Score}";
+        if(scoreText.text != newText)
+        {
+            // scoreText.text = "Score: " + ScoreManager.Instance.Score;
+            // StopCoroutine(TextCountAnimation(scoreText, ScoreManager.Instance.Score));
+
+            StartCoroutine(TextCountAnimation(scoreText, ScoreManager.Instance.Score));
+        }
+    }
+
+    void UpdateComboText()
+    {
         if (ScoreManager.Instance.CurrentCombo != 0)
         {
-            comboText.text = $"Combo: x{ScoreManager.Instance.CurrentCombo}";
+            string newText = $"x{ScoreManager.Instance.CurrentCombo}";
+            if (comboText.text != newText)
+            {
+                comboText.text = newText;
+                StopCoroutine(TextScale(comboText));
+                StartCoroutine(TextScale(comboText));
+            }
         }
         else
         {
             comboText.text = "";
         }
+    }
 
-        multText.text = $"Multiplier: x{ScoreManager.Instance.ScoreMult:F1}";
+    IEnumerator TextScale(TextMeshProUGUI textObject)
+    {
+        textObject.rectTransform.localScale = new(1.1f, 1.1f, 1.1f);
+
+        float time = 0f;
+        while (time < 1.0f)
+        {
+            float temp = Mathf.Lerp(1.1f, 1.0f, time);
+            textObject.rectTransform.localScale = new(temp, temp, temp);
+
+            time += Time.deltaTime / scaleTime;
+            yield return null;
+        }
+        yield return null;
+    }
+    
+    private IEnumerator TextCountAnimation(TextMeshProUGUI textObject, int finalScore)
+    {
+        // int currScore = this.currScore;
+        // int increment = (finalScore - currScore) / AnimIncrements;
+        while (currScore <= finalScore)
+        {
+            currScore += 1;
+            textObject.text = $"Score: {currScore}";
+            yield return new WaitForSeconds(countTime / (float)(finalScore - currScore));
+        }
+        textObject.text = $"Score: {finalScore}"; ;
     }
 }
